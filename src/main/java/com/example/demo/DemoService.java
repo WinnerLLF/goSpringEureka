90e35dab99da1e151975997b64683a7441c62f1b
winnerLLF <1447560092@qq.com>
2019-08-23 17:02:35 +0800

    Update DemoService.java

--- src/main/java/com/example/demo/DemoService.java
+++ src/main/java/com/example/demo/DemoService.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.netflix.sidecar.EnableSidecar;
 @SpringBootApplication
 public class DemoService {
     //【SpringCloud】配置：application.yml
-    // 启动的命令：nohup java -jar ./SeaAreaService-0.0.1-SNAPSHOT.jar >seaArea.out &
     public static void main(String[] args) {
         SpringApplication.run(DemoService.class, args);
     }
